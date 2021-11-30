7a3871f8b9287117f963fec08ec45d2547c1ce07
luchikhin <a.luchikhin@yandex.ru>
2021-11-30 22:50:16 +0300

    nedodelki

--- pages/admin.py
+++ pages/admin.py
@@ -1,5 +1,20 @@
+from ckeditor_uploader import forms
 from django.contrib import admin
 from .models import Page, TopMenu
+
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+class PageAdminForm(forms.ModelForm):
+    body = forms.CharField(widget=CKEditorUploadingWidget())
+    class Meta:
+        model = Page
+
+class PageAdmin(admin.ModelAdmin):
+    form = PageAdminForm
+
+admin.site.register(Page, PageAdmin)
+
 # Register your models here.
 class admPage(admin.ModelAdmin):
     list_display = ('title', 'create', 'update', 'published')
